194111dfeec5364dba24b7b3979838e67825b569
Ilyas156 <ibejsenbi@bk.ru>
2024-03-13 21:40:00 +0500

    update migrations

--- database/migrations/2024_03_13_163838_create_users_table.php
+++ database/migrations/2024_03_13_163838_create_users_table.php
@@ -12,9 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('user', function (Blueprint $table) {
-            $table->id();
-            $table->string('login');
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->unique();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -24,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user');
+        Schema::dropIfExists('users');
     }
 };
